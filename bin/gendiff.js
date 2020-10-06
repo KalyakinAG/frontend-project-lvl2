01c52cd94669f2b1746745c9c6a54c9df655d92f
KalyakinAG <Andrey.Kalyakin@gmail.com>
2020-10-06 21:40:30 +0300

    Добавлены в вывод справки аргументы

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,9 @@ import program from 'commander';
 
 program.version('1.0.0.1');
 program.description('Compares two configuration files and shows a difference.');
-program.option('-d, --debug', 'отладка');
+program.option('-d, --debug', 'debug');
+program.option('-f, --format [type]', 'output format');
+program.arguments('<filepath1> <filepath2>');
+
 program.parse(process.argv);
 if (program.debug) console.log(program.opts());
