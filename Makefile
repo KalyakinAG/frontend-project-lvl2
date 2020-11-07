7308e6d03f7b0c656ef1131303652f15366c2fc4
KalyakinAG <Andrey.Kalyakin@gmail.com>
2020-11-07 08:05:35 +0300

    Упрощена структура тестовых примеров

--- Makefile
+++ Makefile
@@ -1,14 +1,8 @@
-install:
-	npm install
-uninstall:
-	npm uninstall
 publish:
 	npm publish --dry-run
 lint:
 	npx eslint .
 link:
 	npm link
-test:
-	npm test
 test-coverage:
 	npm test -- --coverage --coverageProvider=v8
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,26 +4,12 @@ import genDiff from '../src/index.js';
 
 const fixturePath = path.join(process.cwd(), '__tests__', '__fixtures__');
 
-const tests = [
-  {
-    ext: 'json',
-    format: 'stylish',
-  },
-  {
-    ext: 'json',
-    format: 'plain',
-  },
-  {
-    ext: 'yaml',
-    format: 'stylish',
-  },
-  {
-    ext: 'ini',
-    format: 'stylish',
-  },
-];
+const tests = [];
+['json', 'yaml', 'ini'].forEach((ext) => {
+  ['stylish', 'plain'].forEach((format) => tests.push({ ext, format }));
+});
 
 test.each(tests)('Тест %o', (item) => {
   expect(genDiff(path.join(fixturePath, `file1.${item.ext}`), path.join(fixturePath, `file2.${item.ext}`), item.format))
-    .toEqual(fs.readFileSync(path.join(fixturePath, `expected_file_${item.ext}_${item.format}.txt`), 'utf8'));
+    .toEqual(fs.readFileSync(path.join(fixturePath, `expected_file_${item.format}.txt`), 'utf8'));
 });
