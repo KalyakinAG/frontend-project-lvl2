ec465772bb5a832b5dff7488642987e233e3b0ab
KalyakinAG <Andrey.Kalyakin@gmail.com>
2020-11-16 09:24:56 +0300

    Тесты оформлены через функцию describe.each

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,16 +5,21 @@ import genDiff from '../src/index.js';
 
 const fixturePath = path.join(process.cwd(), '__tests__', '__fixtures__');
 
-const tests = [];
-['json', 'yaml', 'ini'].forEach((ext) => {
-  ['stylish', 'plain'].forEach((format) => tests.push({ ext, format }));
-});
-
-test.each(tests)('Тест %o', (item) => {
-  const filePath1 = path.join(fixturePath, `file1.${item.ext}`);
-  const filePath2 = path.join(fixturePath, `file2.${item.ext}`);
-  const fileExpectedPath = path.join(fixturePath, `expected_file_${item.format}.txt`);
-  const obj1 = parse(path.resolve(process.cwd(), filePath1));
-  const obj2 = parse(path.resolve(process.cwd(), filePath2));
-  expect(genDiff(obj1, obj2, item.format)).toEqual(fs.readFileSync(fileExpectedPath, 'utf8'));
+describe.each`
+  ext    | format | descr
+  ${'json'} | ${'stylish'} | ${'diff for json'}
+  ${'json'} | ${'plain'} | ${'diff for json'}
+  ${'yaml'} | ${'stylish'} | ${'diff for yaml'}
+  ${'yaml'} | ${'plain'} | ${'diff for yaml'}
+  ${'ini'} | ${'stylish'} | ${'diff for ini'}
+  ${'ini'} | ${'plain'} | ${'diff for ini'}
+`(`Params: ${'$ext -format $format'}`, ({ ext, format, descr }) => {
+  test(`${descr}`, () => {
+    const filePath1 = path.join(fixturePath, `file1.${ext}`);
+    const filePath2 = path.join(fixturePath, `file2.${ext}`);
+    const fileExpectedPath = path.join(fixturePath, `expected_file_${format}.txt`);
+    const obj1 = parse(path.resolve(process.cwd(), filePath1));
+    const obj2 = parse(path.resolve(process.cwd(), filePath2));
+    expect(genDiff(obj1, obj2, format)).toEqual(fs.readFileSync(fileExpectedPath, 'utf8'));
+  });
 });
