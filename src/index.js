1349735a98e7c03ca2db9336adad3c307ab8ae33
KalyakinAG <Andrey.Kalyakin@gmail.com>
2020-12-09 11:39:55 +0300

    Переименование функции

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import format from './formatters/index.js';
 import parse from './parsers.js';
 import genPropertyDiff from './diff.js';
 
-const fileType = (filepath) => path.extname(filepath).substring(1);
+const getFileType = (filepath) => path.extname(filepath).substring(1);
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const obj1 = parse(fs.readFileSync(filepath1, 'utf8'), fileType(filepath1));
-  const obj2 = parse(fs.readFileSync(filepath2, 'utf8'), fileType(filepath2));
+  const obj1 = parse(fs.readFileSync(filepath1, 'utf8'), getFileType(filepath1));
+  const obj2 = parse(fs.readFileSync(filepath2, 'utf8'), getFileType(filepath2));
   const diff = genPropertyDiff(obj1, obj2);
   return format(diff, formatName);
 };
