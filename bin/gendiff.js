4f025292c3a83a48f68a4cc1e292151fc976810f
KalyakinAG <Andrey.Kalyakin@gmail.com>
2020-12-13 16:25:14 +0300

    Применен текучий интерфейс, убран ключ отладки

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,16 +3,14 @@
 import program from 'commander';
 import genDiff from '../src/index.js';
 
-program.version('1.0.0');
-program.description('Compares two configuration files and shows a difference.');
-program.option('-d, --debug', 'debug');
-program.option('-f, --format [type]', 'output format', 'stylish');
-program.arguments('<filepath1> <filepath2>');
-program.action((filepath1, filepath2, cmdObj) => {
-  console.log(genDiff(filepath1, filepath2, cmdObj.format));
-});
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-d, --debug', 'debug')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, cmdObj) => {
+    console.log(genDiff(filepath1, filepath2, cmdObj.format));
+  });
 
 program.parse(process.argv);
-if (program.debug) {
-  console.log(`Current directory: ${process.cwd()}`);
-}
